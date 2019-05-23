97955e4a1f2e4db723e0a8c3be2080a8bcb45ff2
Eric Stolten <stoltene2@gmail.com>
2019-05-23 19:40:55 -0400

    Add matrix library

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [net.mikera/core.matrix "0.62.0"]
+                 [net.mikera/vectorz-clj "0.48.0"]]
   :main ^:skip-aot trace-a-ray.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
