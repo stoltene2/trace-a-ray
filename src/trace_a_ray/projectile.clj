8e66298fa6991316af7c7f20fdcfd703bc21a138
Eric Stolten <stoltene2@gmail.com>
2019-05-23 19:40:36 -0400

    Create a graph of a projectile and save to ppm format

--- src/trace_a_ray/projectile.clj
+++ src/trace_a_ray/projectile.clj
@@ -1,5 +1,7 @@
 (ns trace-a-ray.projectile
-  (:require [trace-a-ray.tuple :as t]))
+  (:require [trace-a-ray.tuple :as t]
+            [trace-a-ray.canvas :as canvas]
+            [trace-a-ray.color :as color]))
 
 
 (defn projectile [position velocity]
@@ -21,10 +23,38 @@ POSITION is a point and VELOCITY is a vector."
 (defn run-it []
   "Execute tick repeatedly until projectile hits the ground (-y value).
 The environment has an effect from gravity and wind."
-  (let [env    {:gravity (t/vector 0 -0.1 0) :wind (t/vector -0.1, 0, 0)}
-        p-init [(t/point 0 1 0) (t/normalize (t/vector 1 3 0))]]
-    (loop [tick-count            0
-           p p-init]
+  (let [env    {:gravity (t/vector 0 -0.01 0) :wind (t/vector -0.004, 0, 0)}
+        arbitrary-scale 2.1
+        starting-point (t/point 0 0 0)
+        starting-velocity (t/* arbitrary-scale (t/normalize (t/vector 20 30 0)))
+        p-init [starting-point starting-velocity]]
+    (loop [tick-count 0
+           p p-init
+           points []]
       (if (< (y-pos p) 0)
-        [tick-count (first p)]
-        (recur (inc tick-count) (tick env p))))))
+        [tick-count (first p) points]
+        (recur (inc tick-count) (tick env p) (conj points (first p)))))))
+
+(defn in-interval [n lower upper]
+  "Returns true if lower <= n <= upper"
+  (and (>= n lower) (<= n upper)))
+
+(defn make-ppm []
+  (let [max-x 200
+        max-y 200
+        canvas (canvas/canvas max-x max-y)
+        point-to-pixel (fn [x]
+                         [(Math/round (float (first x)))
+                          (Math/round (float (- (dec max-y) (second x))))])
+
+        points (filter (fn [[x y]] (and (in-interval x 0 (dec max-x))
+                                        (in-interval y 0 (dec max-y)))) (map point-to-pixel (nth (run-it) 2)))
+        red (color/color 255 0 0)]
+    (loop [canvas canvas
+           points (rest points)
+           p (first points)]
+      (if (empty? points)
+        (canvas/canvas-to-ppm canvas)
+        (recur (canvas/write-pixel (first p) (second p) red canvas)
+               (rest points)
+               ((comp first rest) points))))))
