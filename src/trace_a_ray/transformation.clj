7c71aa918314d167bd26e754b0562cca16e0c4b1
Eric Stolten <stoltene2@gmail.com>
2019-05-25 10:13:19 -0400

    Add transformations for x, y, and z rotations

--- src/trace_a_ray/transformation.clj
+++ src/trace_a_ray/transformation.clj
@@ -1,9 +1,6 @@
 (ns trace-a-ray.transformation
   (:require [clojure.core.matrix :as matrix]))
 
-;; TODO: Where do I set this?
-;;(set-current-implementation :vectorz)
-
 (defn translate [x y z]
   "Create a matrix transformation which will move all points by x, y,
   and z."
@@ -20,3 +17,25 @@
    [0 y 0 0]
    [0 0 z 0]
    [0 0 0 1]])
+
+
+(defn rotate-x [rad]
+  "Rotate point around x-axis by rad radians."
+  [[1 0              0                  0]
+   [0 (Math/cos rad) (- (Math/sin rad)) 0]
+   [0 (Math/sin rad) (Math/cos rad)     0]
+   [0 0              0                  1]])
+
+(defn rotate-y [rad]
+  "Rotate point around y-axis by rad radians."
+  [[(Math/cos rad)     0   (Math/sin rad)     0]
+   [0                  1   0                  0]
+   [(- (Math/sin rad)) 0   (Math/cos rad)     0]
+   [0                  0   0                  1]])
+
+(defn rotate-z [rad]
+  "Rotate point around z-axis by rad radians."
+  [[(Math/cos rad) (- (Math/sin rad)) 0 0]
+   [(Math/sin rad) (Math/cos rad)     0 0]
+   [0              0                  1 0]
+   [0              0                  0 1]])
--- test/trace_a_ray/transformation_test.clj
+++ test/trace_a_ray/transformation_test.clj
@@ -11,6 +11,7 @@
   EPSILON."
   (let [components (map vector (flatten m1) (flatten m2))
         within-epsilon (fn [[x y]] (< (Math/abs (- x y)) EPSILON))]
+
     (every? identity (map within-epsilon components))))
 
 
@@ -18,7 +19,9 @@
   (testing "When we want to translate a point"
     (let [trans (transform/translate 5 -3 2)
           p (tuple/point -3 4 5)]
-      (is (matrix= (m/mmul trans p) (tuple/point 2 1 7)) "Points are moved by translation")))
+
+      (is (matrix= (m/mmul trans p) (tuple/point 2 1 7))
+          "Points are moved by translation")))
 
   (testing "When we want to translate a point back"
     (let [trans (transform/translate 5 -3 2)
@@ -26,7 +29,9 @@
           initial-p (tuple/point -3 4 5)
           moved-p (m/mmul trans initial-p)
           expected-p (tuple/point -3 4 5)]
-      (is (matrix= (m/mmul inverse-trans moved-p) expected-p) "Points are moved by translation")))
+
+      (is (matrix= (m/mmul inverse-trans moved-p) expected-p)
+          "Points are moved by translation")))
 
   (testing "When translating a vector it is unaffected by the transform operation."
     (let [v (tuple/vector -3 4 5)
@@ -37,19 +42,98 @@
   (testing "When scaling a point"
     (let [trans (transform/scale 2 3 4)
           p (tuple/point -4 6 8)]
-      (is (matrix= (m/mmul trans p) (tuple/point -8 18 32)) "Components are multiplied")))
+
+      (is (matrix= (m/mmul trans p) (tuple/point -8 18 32))
+          "Components are multiplied")))
 
   (testing "When shrinking a point we can use the inverse"
     (let [inverse-trans (m/inverse (transform/scale 2 3 4))
           p (tuple/point -4 6 8)]
-      (is (matrix= (m/mmul inverse-trans p) (tuple/point -2 2 2)) "Components are shrunk")))
+
+      (is (matrix= (m/mmul inverse-trans p) (tuple/point -2 2 2))
+          "Components are shrunk")))
 
   (testing "When reflecting a point we can use negative values"
     (let [trans (m/inverse (transform/scale -1 1 1))
           p (tuple/point -4 6 8)]
-      (is (matrix= (m/mmul trans p) (tuple/point 4 6 8)) "x-component flips")))
+
+      (is (matrix= (m/mmul trans p) (tuple/point 4 6 8))
+          "x-component flips")))
 
   (testing "When scaling a vector"
     (let [trans (transform/scale 2 3 4)
           v (tuple/vector -4 6 8)]
-      (is (matrix= (m/mmul trans v) (tuple/vector -8 18 32)) "Vectors are scaled too"))))
+
+      (is (matrix= (m/mmul trans v) (tuple/vector -8 18 32))
+          "Vectors are scaled too"))))
+
+
+
+;;
+;;                  |+y
+;;                  |
+;;                  |
+;;                  |
+;;                  |
+;;                  |
+;;                  |
+;;                  |
+;;                  o------------- +z
+;;                   \
+;;                    \
+;;                     \
+;;                      \
+;;                       \
+;;                        \
+;;                          +x
+;;
+;;
+
+(deftest rotating
+  (testing "When rotating around the x-axis"
+    (let [original-p (tuple/point 0 1 0)
+          sqrt2-div-2 (/ (Math/sqrt 2) 2)
+          expected (tuple/point 0.0 sqrt2-div-2 sqrt2-div-2)
+          rot-45-deg (transform/rotate-x (/ Math/PI 4))
+          rot-90-deg (transform/rotate-x (/ Math/PI 2))]
+
+      (is (matrix= expected (m/mmul rot-45-deg original-p))
+          "Rotate by 45-deg")
+
+      (is (matrix= original-p (m/mmul (m/inverse rot-45-deg) expected))
+          "Rotate back by 45-deg")
+
+      (is (matrix= (tuple/point 0 0 1) (m/mmul rot-90-deg original-p))
+          "Rotate by 90-deg, y falls onto z.")))
+
+  (testing "When rotating around the y-axis"
+    (let [original-p (tuple/point 0 0 1)
+          sqrt2-div-2 (/ (Math/sqrt 2) 2)
+          expected (tuple/point sqrt2-div-2 0.0 sqrt2-div-2)
+          rot-45-deg (transform/rotate-y (/ Math/PI 4))
+          rot-90-deg (transform/rotate-y (/ Math/PI 2))]
+
+      (is (matrix= expected (m/mmul rot-45-deg original-p))
+          "Rotate 45 degrees from x to z.")
+
+      (is (matrix= original-p (m/mmul (m/inverse rot-45-deg) expected))
+          "Rotate 45 degrees back from z to x.")
+
+      (is (matrix= (tuple/point 1 0 0) (m/mmul rot-90-deg original-p))
+          "Rotate by 90-deg, z falls to x")))
+
+  (testing "When rotating around the z-axis"
+    (let [original-p (tuple/point 0 1 0)
+          sqrt2-div-2 (/ (Math/sqrt 2) 2)
+          expected (tuple/point (- sqrt2-div-2) sqrt2-div-2 0.0)
+          rot-45-deg (transform/rotate-z (/ Math/PI 4))
+          rot-90-deg (transform/rotate-z (/ Math/PI 2))]
+
+      (is (matrix= expected (m/mmul rot-45-deg original-p))
+          "Rotate 45 degrees from y onto -x.")
+
+      (is (matrix= original-p (m/mmul (m/inverse rot-45-deg) expected))
+          "Rotate 45 degrees from -x towards y.")
+
+      (is (matrix= (tuple/point -1 0 0) (m/mmul rot-90-deg original-p))
+          "Rotate by 90-deg, y falls to -x because of right hand rule."))))
