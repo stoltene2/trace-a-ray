a5586b546808b68957771e196683e5881895519a
Eric Stolten <stoltene2@gmail.com>
2019-06-04 07:04:20 -0400

    Determine if a ray intersects a sphere

--- src/trace_a_ray/ray.clj
+++ src/trace_a_ray/ray.clj
@@ -7,3 +7,30 @@
   "Determine the position of the ray at time t"
 
   (t/+ (.point ray) (t/* time (.direction ray))))
+
+
+(defn intersect [sphere ray]
+  "Given a unit-sphere and a ray, determine if there are any
+  intersections between the two."
+
+  (let [world-origin (t/point 0 0 0)
+        ray-dir (.direction ray)
+        sphere->ray-v (t/- (.point ray) world-origin)
+
+        a (t/dot ray-dir ray-dir)
+        b (* 2 (t/dot ray-dir sphere->ray-v))
+        c (dec (t/dot sphere->ray-v sphere->ray-v))
+
+        discriminate (- (* b b) (* 4 a c))
+
+        has-roots? (>= discriminate 0)]
+
+    (if has-roots?
+
+      (let [discriminiate-sqrt (Math/sqrt discriminate)
+            two-a (* 2 a)
+            t1 (/ (- (- b) discriminiate-sqrt) two-a)
+            t2 (/ (+ (- b) discriminiate-sqrt) two-a)]
+        (if (< t1 t2) [t1 t2] [t2 t1]))
+
+      [])))
--- test/trace_a_ray/ray_test.clj
+++ test/trace_a_ray/ray_test.clj
@@ -1,4 +1,4 @@
-(ns traca-a-ray.ray-test
+(ns trace-a-ray.ray-test
   (:require [trace-a-ray.ray :as ray]
             [trace-a-ray.tuple :as tuple]
             [clojure.test :refer :all]))
@@ -32,3 +32,43 @@
 
       (is (= (ray/position r 2.5) (tuple/point 4.5 3.0 4.0))
           "Time 2.5"))))
+
+(defn sphere []
+  "Create a unit-sphere."
+  [])
+
+(deftest ray-sphere-intersection
+  (testing "When a ray intersects a sphere in two points."
+    (let [s (sphere)
+          p (tuple/point 0 0 -5)
+          dir (tuple/vector 0 0 1)
+          r (ray/->ray p dir)]
+      (is (= [4.0 6.0] (ray/intersect s r)))))
+
+  (testing "When a ray intersects a sphere tangentially"
+    (let [s (sphere)
+          p (tuple/point 0 1 -5)
+          dir (tuple/vector 0 0 1)
+          r (ray/->ray p dir)]
+      (is (= [5.0 5.0] (ray/intersect s r)))))
+
+  (testing "When a ray misses a sphere."
+    (let [s (sphere)
+          p (tuple/point 0 2 -5)
+          dir (tuple/vector 0 0 1)
+          r (ray/->ray p dir)]
+      (is (= [] (ray/intersect s r)))))
+
+  (testing "When a ray has the point in the interior of a sphere."
+    (let [s (sphere)
+          p (tuple/point 0 0 0)
+          dir (tuple/vector 0 0 1)
+          r (ray/->ray p dir)]
+      (is (= [-1.0 1.0] (ray/intersect s r)))))
+
+  (testing "When a ray is in front of a sphere and points are in increasing order."
+    (let [s (sphere)
+          p (tuple/point 0 0 5)
+          dir (tuple/vector 0 0 1)
+          r (ray/->ray p dir)]
+      (is (= [-6.0 -4.0] (ray/intersect s r))))))
