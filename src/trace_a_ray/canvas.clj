e171905674270d6abab9763ad3e1f9d05a971e0a
Eric Stolten <stoltene2@gmail.com>
2023-05-12 17:27:10 -0400

    Add comments about PPM performance improvements

--- src/trace_a_ray/canvas.clj
+++ src/trace_a_ray/canvas.clj
@@ -1,17 +1,16 @@
 (ns trace-a-ray.canvas
   (:require [trace-a-ray.color :as color]))
 
+;; I should use a map here where the key is [x y] so that it is sparse
 (defn canvas [x y]
   "Creates an empty canvas where all values are black by default"
-  ;; (->>
-  ;;  (color/color 0 0 0)
-  ;;  (repeat x)
-  ;;  (repeat y))
   (vec (repeat y (vec (repeat x (color/color 0 0 0))))))
 
+;; Not sure this is used
 (defn pixel-at [x y c]
   (nth (nth c y) x))
 
+;; This doesn't need to be public
 (defn write-pixel [x y color canv]
   "Create a new canvas where coordinate (x, y) is set to color."
   ;; (assoc map key val)
@@ -20,13 +19,15 @@
 (defn- clamp [x] (if (< x 0.0) 0.0 (if (> x 1.0) 1.0 x)))
 (defn- scale [x] (Math/round (float (* x 255))))
 
+
+;; canvas->ppm could exist in a new module called `trace-a-ray.ppm`
+;; I can use a transient string or a string builder `with-out-string`
 (defn canvas-to-ppm [c]
   "Convert a canvas to a PPM image string"
-  (let [x (count (first c))
-        y (count c)
-        max-color-value 255
-        row-to-ppm (fn [r] (apply str (interpose " " (map (comp scale clamp) (flatten r)))))
-        ppm-rows (map row-to-ppm c)]
+  (let y (count c)
+       max-color-value 255
+       row-to-ppm (fn [r] (apply str (interpose " " (map (comp scale clamp) (flatten r)))))
+       ppm-rows (map row-to-ppm c)
     (apply str
            (concat (interpose "\n"
                        (concat ["P3"
