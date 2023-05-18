9a700a7548c9fe1cfa81881dd99b3075746f4b84
Eric Stolten <stoltene2@gmail.com>
2023-05-18 08:03:25 -0400

    Add a timeit macro that behaves similar to time
    timeit will swallow the results of the computation instead of dumping
    it to the repl

---
+++ src/user.clj
@@ -0,0 +1,12 @@
+(ns user)
+
+(defmacro timeit
+  "Like the standard time macro but it returns nil instead of what is
+  evaluated. This is useful if you want to gather rough timing of a
+  form execution and you don't want to blow the repl up with the
+  results."
+  [form]
+  `(time
+    (do
+      ~form
+      nil)))
